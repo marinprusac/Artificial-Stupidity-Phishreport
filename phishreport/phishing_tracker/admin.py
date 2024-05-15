ad5be3ddc5778043032fc227531ff2e9f0160b39
PD <62700665+G-rox-y@users.noreply.github.com>
2024-05-15 16:53:21 +0200

    Dodani modeli u admin site

--- phishreport/phishing_tracker/admin.py
+++ phishreport/phishing_tracker/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import PhishingEventModel
+admin.site.register(PhishingEventModel)
+
+from .models import AnalystCommentModel
+admin.site.register(AnalystCommentModel)
