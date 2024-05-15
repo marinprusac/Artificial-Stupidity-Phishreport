69e61da290657f610069b5e2523044ed7c8a91f0
Marin Prusac <prusac.marin@gmail.com>
2024-05-15 20:20:51 +0200

    Form post functionality

--- phishreport/phishing_tracker/views.py
+++ phishreport/phishing_tracker/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 
 # Create your views here.
-def index(request):
+def index(request: HttpRequest):
     return render(request, 'index.html', None)
 
-def report(request):
-    return render(request, 'report/report.html', None)
+def report(request: HttpRequest):
+    if request.method == "POST":
+        pass
+        return render(request, 'report/report.html', None)
+    else:
+        return render(request, 'report/report.html', None)
